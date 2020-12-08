c2d34dd7eb0247bb381aa1fb08b0b6d66ca3f773
孙 <sun.youwei@99cloud.net>
2020-12-08 15:17:32 +0800

    再次进入配置部署文件步骤时，优化展示的文案

--- src/pages/workspace/ConfigYaml.vue
+++ src/pages/workspace/ConfigYaml.vue
@@ -61,7 +61,12 @@
           </a>
         </el-upload>
 
-        <div v-show="hasValidate">
+        <div v-show="appYamlFileId && hasInited">
+          <div class="green test tit">
+            {{ $t('workspace.configYaml.pass') }}
+          </div>
+        </div>
+        <div v-show="!hasInited && hasValidate">
           <div :class="appYamlFileId ? 'green test tit' : 'red test tit'">
             {{ appYamlFileId ? $t('workspace.configYaml.pass') : $t('workspace.configYaml.fail') }}
           </div>
@@ -149,6 +154,7 @@ export default {
       yamlFileList: [],
       activeName: 'first',
       hasValidate: false,
+      hasInited: false,
       userId: sessionStorage.getItem('userId'),
       projectId: sessionStorage.getItem('mecDetailID')
     }
@@ -187,6 +193,9 @@ export default {
     },
     // 移除Yaml文件
     removeUploadyaml (file, fileList) {
+      if (this.hasInited) {
+        this.hasInited = false
+      }
       Workspace.deleteYamlFileApi(this.appYamlFileId)
       this.hasValidate = false
       this.yamlFileList = []
@@ -195,6 +204,9 @@ export default {
     },
     // 上传Yaml文件
     submitYamlFile (yamlFileList) {
+      if (this.hasInited) {
+        this.hasInited = false
+      }
       this.uploadYamlLoading = true
       let fd = new FormData()
       fd.append('file', yamlFileList[0])
@@ -235,6 +247,7 @@ export default {
             const fileObj = res.data.find(s => s.fileId === this.projectBeforeConfig.deployFileId) || {}
             if (fileObj.fileId) {
               this.checkFlag = { formatSuccess: true, imageSuccess: true, mepAgentSuccess: true, serviceSuccess: true }
+              this.hasInited = true
               this.hasValidate = true
               this.appYamlFileId = fileObj.fileId
               this.yamlFileList = [{ name: fileObj.fileName, fileId: fileObj.fileId }]
