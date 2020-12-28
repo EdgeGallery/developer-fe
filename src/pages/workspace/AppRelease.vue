11c4d553fef70735490b43eaf028dfec4e11d697
zoulingli <zll_1208@126.com>
2020-12-28 16:04:22 +0800

    修改保存atpTest数据

--- src/pages/workspace/AppRelease.vue
+++ src/pages/workspace/AppRelease.vue
@@ -764,7 +764,9 @@ export default {
     getReleaseConfigFirst () {
       Workspace.getReleaseConfigApi(this.projectId).then(res => {
         let releaseId = res.data.releaseId
-        this.trafficAllData.atpTest = res.data.atpTest
+        if (res.data.atpTest.id) {
+          this.trafficAllData.atpTest = res.data.atpTest
+        }
         Workspace.saveRuleConfig(this.projectId, this.trafficAllData, releaseId)
       })
     },
