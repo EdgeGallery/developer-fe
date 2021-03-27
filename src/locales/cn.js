ab0778f0755cbc2b53510da1f5659df69b8ea266
周文敬 <942517636@qq.com>
2021-03-27 17:36:09 +0800

    能力中心开发者实践区域界面优化

--- src/locales/cn.js
+++ src/locales/cn.js
@@ -118,7 +118,7 @@ const cn = {
     serviceDoc: '服务文档',
     onlineEmulator: '在线模拟器',
     videoTutorial: '视频教程课堂专区',
-    developerCommunity: '开发者问答社区',
+    developerCommunity: '问答社区',
     Detail: '查看详情',
     videoTutorialDetail: '指导开发者基于EdgeGallery平台进行app应用开发、部署调测、测试认证以及应用发布的全流程。',
     developerCommunityDetail: '用于开发者的交流和学习。',
