af5fb9c4a0e6c8ec359d141f5de42671c77a6c6a
zoulingli <zll_1208@126.com>
2021-05-28 10:31:39 +0800

    虚拟机部署调测接口联调

--- src/pages/workspace/vmdebug/Main.vue
+++ src/pages/workspace/vmdebug/Main.vue
@@ -19,12 +19,94 @@
     id="div_deploydebug_vm"
     class="deploydebug_vm"
   >
+    <el-card
+      class="box-card"
+      v-loading="vmDataLoading"
+    >
+      <div
+        slot="header"
+        class="clearfix"
+      >
+        <table style="width:100%">
+          <caption />
+          <tr>
+            <th scope="heads" />
+            <th scope="heads" />
+          </tr>
+          <tr>
+            <td>
+              <span class="resCardTitle">{{ $t('workspace.deployDebugVm.deployResultList') }}</span>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <el-row
+        :gutter="24"
+        v-for="(item,index) in deployListData"
+        :key="index"
+      >
+        <el-col :span="13">
+          <div>
+            <el-form
+              label-width="120px"
+              class="formDetail"
+            >
+              <el-form-item :label="$t('workspace.deployDebugVm.vmNameLbl')">
+                {{ item.vmName }}
+              </el-form-item>
+              <el-form-item label="mecHost">
+                {{ item.host?item.host.mecHost:null }}
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-col>
+        <el-col :span="11">
+          <div>
+            <el-form
+              label-width="120px"
+              class="formDetail"
+            >
+              <el-form-item :label="$t('workspace.deployDebugVm.vmStatusLbl')">
+                <em
+                  v-if="item.status==='CREATING'"
+                  class="el-icon-loading deploying icon"
+                />
+                <em
+                  v-if="item.status==='SUCCESS'"
+                  class="el-icon-success success icon"
+                />
+                <em
+                  v-if="item.status==='FAILED'"
+                  class="el-icon-error error icon"
+                />
+                {{ item.status }}
+              </el-form-item>
+              <el-form-item :label="$t('workspace.deployDebugVm.vmApplyTimeLbl')">
+                {{ item.createTime }}
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-col>
+        <el-col :span="24">
+          <p class="operation_btn">
+            <el-button
+              type="text"
+              class="funcBtn"
+              @click="deleteVmDeployList(item)"
+              :disabled="item.status==='CREATING'"
+            >
+              {{ $t('workspace.deployDebugVm.deleteBtnLbl') }}
+            </el-button>
+          </p>
+        </el-col>
+      </el-row>
+    </el-card>
     <el-card class="addRes-box-card">
       <el-button
         class="btn-addRes"
         type="primary"
         @click="deployTest"
-        :disabled="deployListData.length!==0"
+        :disabled="isDisabled || deployListData.length!==0"
       >
         {{ $t('workspace.startDeployment') }}
       </el-button>
@@ -45,23 +127,127 @@ export default {
   data () {
     return {
       userId: sessionStorage.getItem('userId'),
-      deployListData: []
+      deployListData: [],
+      interval: null,
+      vmDataLoading: true,
+      isDisabled: false
     }
   },
   methods: {
     deployTest () {
-      vmService.vmDeployTestApi(this.projectId, this.userId).then(res => {
-        console.log(res.data)
+      this.vmDataLoading = true
+      vmService.vmDeployTestApi(this.projectId, this.userId).then(() => {
+        this.isDisabled = true
+        this.interval = setInterval(() => {
+          this.getProjectVmList()
+        }, 2000)
+      }).catch(err => {
+        this.isDisabled = false
+        if (err.response.data.message === 'Can not get vm package config') {
+          this.$message.warning(this.$t('workspace.deployDebugVm.resourceConfigPromt'))
+        }
       })
+    },
+    getProjectVmList () {
+      vmService.getProjectVmResList(this.projectId, this.userId).then(res => {
+        this.deployListData = []
+        if (res.data.length === 0) {
+          this.clearInterval()
+        } else {
+          let _data = res.data[0]
+          if (_data.status !== 'CREATING') {
+            this.clearInterval()
+          }
+          this.deployListData.push(_data)
+          if (_data.createTime !== null) {
+            _data.createTime = this.dateChange(_data.createTime)
+          }
+        }
+        this.vmDataLoading = false
+      })
+    },
+    deleteVmDeployList (item) {
+      this.$confirm(this.$t('workspace.deployDebugVm.deleteVmDeployList'), this.$t('promptMessage.prompt'), {
+        confirmButtonText: this.$t('common.confirm'),
+        cancelButtonText: this.$t('common.cancel'),
+        type: 'warning'
+      }).then(() => {
+        vmService.deleteVmDeployListApi(this.projectId, item.vmId, this.userId).then(() => {
+          this.$message.success(this.$t('devTools.deleteSucc'))
+          this.getProjectVmList()
+          this.isDisabled = false
+        }).catch(() => {
+          this.$message.error(this.$t('devTools.deleteFail'))
+          this.isDisabled = false
+        })
+      })
+    },
+    dateChange (dateStr) {
+      if (dateStr) {
+        let date = new Date(Date.parse(dateStr))
+        let Y = date.getFullYear()
+        let M = date.getMonth() + 1
+        let D = date.getDate()
+        let H = date.getHours()
+        let m = date.getMinutes()
+        let s = date.getSeconds()
+        return Y +
+          '-' +
+          (M > 9 ? M : '0' + M) +
+          '-' +
+          (D > 9 ? D : '0' + D) +
+          ' ' +
+          (H > 9 ? H : '0' + H) +
+          ':' +
+          (m > 9 ? m : '0' + m) +
+          ':' +
+          (s > 9 ? s : '0' + s)
+      }
+    },
+    clearInterval () {
+      clearTimeout(this.interval)
+      this.interval = null
     }
   },
   mounted () {
+    this.getProjectVmList()
+    this.interval = setInterval(() => {
+      this.getProjectVmList()
+    }, 2000)
   }
 }
 </script>
 
 <style lang='less'>
 .deploydebug_vm {
-  padding: 0px 100px 0px 100px;
+  .resCardTitle{
+    display:block;
+    white-space:nowrap;
+    overflow:hidden;
+    text-overflow:ellipsis;
+    position:relative;
+  }
+  .formDetail{
+    .el-form-item{
+      margin-bottom:0px!important ;
+      .el-form-item__label{
+        color: #adb0b8;
+        padding-right: 20px;
+      }
+      .el-form-item__content{
+        line-height: 40px;
+      }
+    }
+    .vm_log .el-form-item__content{
+      max-height: 80px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      text-overflow: -o-ellipsis-lastline;
+      display: -webkit-box;
+      line-clamp:2;
+      -webkit-line-clamp:2;
+      -webkit-box-orient:vertical;
+    }
+  }
 }
 </style>
