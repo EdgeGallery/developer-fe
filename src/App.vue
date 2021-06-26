d7f6fcd42a73ec690446796e452e5e6ef4f90fbe
zoulingli <zll_1208@126.com>
2021-06-26 08:57:46 +0800

    解决刷新页面底部组件闪烁问题

--- src/App.vue
+++ src/App.vue
@@ -25,92 +25,24 @@
       class="clearfix"
     />
     <router-view />
-    <EgFooter
-      v-if="platformData[0].url!==''"
-      :language="language"
-      :platform-data="platformData"
-      :home-page="homePage"
-      :specific-bg="specificBg"
-      specific-bg-color="#ffffff"
-    />
   </div>
 </template>
 
 <script>
 import Navcomp from './components/common/Nav.vue'
-import EgFooter from 'eg-view/src/components/EgFooter.vue'
 export default {
   name: 'App',
   components: {
-    Navcomp,
-    EgFooter
+    Navcomp
   },
   data () {
     return {
-      language: localStorage.getItem('language'),
-      scrollTop: 0,
-      appStoreUrl: '',
-      atpUrl: '',
-      mecmUrl: '',
-      platformData: [
-        {
-          name: 'AppStore',
-          url: ''
-        },
-        {
-          name: 'ATP',
-          url: ''
-        },
-        {
-          name: 'MECM',
-          url: ''
-        }
-      ],
-      homePage: true,
-      specificBg: true
-    }
-  },
-  watch: {
-    '$i18n.locale': function () {
-      this.language = localStorage.getItem('language')
-    },
-    $route (to, from) {
-      if (to.path === '/mecDeveloper' || to.path === '/') {
-        this.homePage = true
-      } else {
-        this.homePage = false
-      }
-      if (to.path === '/mecDeveloper' || to.path === '/' || to.path === '/mecDeveloper/api/mep') {
-        this.specificBg = true
-      } else {
-        this.specificBg = false
-      }
+      scrollTop: 0
     }
   },
   methods: {
     getScrollTop () {
       this.scrollTop = this.$refs.app.getBoundingClientRect().top
-    },
-    getPlatformUrl () {
-      let currUrl = window.location.origin
-      if (currUrl.indexOf('30092') !== -1) {
-        this.appStoreUrl = currUrl.replace('30092', '30091')
-        this.atpUrl = currUrl.replace('30092', '30094')
-        this.mecmUrl = currUrl.replace('30092', '30093')
-      } else {
-        this.appStoreUrl = currUrl.replace('developer', 'appstore')
-        this.atpUrl = currUrl.replace('developer', 'atp')
-        this.mecmUrl = currUrl.replace('developer', 'mecm')
-      }
-      this.platformData.forEach(item => {
-        if (item.name === 'AppStore') {
-          item.url = this.appStoreUrl
-        } else if (item.name === 'ATP') {
-          item.url = this.atpUrl
-        } else if (item.name === 'MECM') {
-          item.url = this.mecmUrl
-        }
-      })
     }
   },
   mounted () {
@@ -120,7 +52,6 @@ export default {
         this.getScrollTop()
       })()
     }
-    this.getPlatformUrl()
   },
   beforeDestroy () {
     window.removeEventListener('scroll', this.getScrollTop, true)
