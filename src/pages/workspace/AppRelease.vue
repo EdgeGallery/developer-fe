6bf604168280499129e01a0375b6f9cac36a3432
qujingjing <qujingjing1@huawei.com>
2021-08-23 20:42:47 +0800

    英文下按钮显示适配

--- src/pages/workspace/AppRelease.vue
+++ src/pages/workspace/AppRelease.vue
@@ -379,10 +379,10 @@
             </div>
           </div>
         </div>
-        <div class="elButton step1 defaultFontLight">
+        <div class="step1 defaultFontLight">
           <el-button
             v-if="active===0"
-            type="primary"
+            class="work-button"
             @click="saveConfig"
             :disabled="isRelease"
           >
@@ -390,7 +390,7 @@
           </el-button>
           <el-button
             v-if="active===0"
-            type="primary"
+            class="work-button"
             @click="appDetaildialog=true"
             :disabled="isRelease"
           >
@@ -1240,6 +1240,13 @@ export default {
       padding: 0 30px 0 39px;
       .step1 {
         margin: 40px 0 0 !important;
+        text-align: right;
+        .work-button {
+          margin-left: 30px;
+        }
+        .work-button.el-button.is-disabled, .work-button.el-button.is-disabled:hover {
+          background-color: #5844be;
+        }
       }
     }
     .second-step {
@@ -1249,7 +1256,6 @@ export default {
         font-size: 16px;
         float: right;
         background-color: #5844be !important;
-        width: 140px;
         height: 39px;
         border-radius: 8px;
         font-family: defaultFontLight !important;
