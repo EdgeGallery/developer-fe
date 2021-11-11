e82e3a39af330b343b9373f257950d1f6c9fe09a
15091636210 <15091636210@163.com>
2021-11-11 15:04:01 +0800

    分发接口列表

--- edgegallery-fe/src/pages/mecm/AppDeployDetailList.vue
+++ edgegallery-fe/src/pages/mecm/AppDeployDetailList.vue
@@ -17,7 +17,7 @@
           v-loading="dataLoading"
           class="common-table"
           :data="currPageTableData"
-          :default-sort="{prop:'name',order:'descending'}"
+          :default-sort="{ prop: 'name', order: 'descending' }"
         >
           <el-table-column
             prop="name"
@@ -89,35 +89,51 @@ import Pagination from '../../components/Pagination.vue'
 import Search from '../../components/Search.vue'
 export default {
   components: {
-    Pagination, Search
+    Pagination,
+    Search
   },
   data () {
     return {
-      dataLoading: true,
+      dataLoading: false,
       appType: '',
       language: localStorage.getItem('language'),
       tableData: [],
       paginationData: [],
-      currPageTableData: [],
+      currPageTableData: [
+        {
+          name: '位置服务',
+          version: 'v1.0',
+          provider: 'EdgeGallery',
+          type: 'Video',
+          industry: 'Smark Park',
+          affinity: 'X86',
+          appstoreName: 'appstore',
+          syncTime: '2021-10-21'
+        }
+      ],
       serchData: null
     }
   },
   mounted () {
-    this.appType = this.$route.query.type ? this.$route.query.type : ''
-    this.getDeployListData()
+    // this.appType = this.$route.query.type ? this.$route.query.type : ''
+    // this.getDeployListData()
   },
-  watch: {
-    '$i18n.locale': function () {
-      let language = localStorage.getItem('language')
-      this.language = language
-      this.getDeployListData()
-    }
-  },
+  // watch: {
+  //   '$i18n.locale': function () {
+  //     let language = localStorage.getItem('language')
+  //     this.language = language
+  //     this.getDeployListData()
+  //   }
+  // },
   methods: {
     filterTableData (val) {
-      this.paginationData = this.paginationData.filter(item => {
-        return Object.keys(item).some(key => {
-          return String(item[key]).toLowerCase().indexOf(val) > -1
+      this.paginationData = this.paginationData.filter((item) => {
+        return Object.keys(item).some((key) => {
+          return (
+            String(item[key])
+              .toLowerCase()
+              .indexOf(val) > -1
+          )
         })
       })
     },
@@ -133,60 +149,63 @@ export default {
       }
     },
     checkDetail (row) {
-      sessionStorage.setItem('appRow', JSON.stringify(row))
-      sessionStorage.setItem('appId', row.appId)
+      // sessionStorage.setItem('appRow', JSON.stringify(row))
+      // sessionStorage.setItem('appId', row.appId)
       this.$nextTick(
-        this.$router.push('/EG/mecm/distribute?appId=' + row.appId)
+        this.$router.push('/EG/mecm/distribute')
       )
     },
     getDeployListData () {
-      appDeploy.initApmPackages().then(response => {
-        this.tableData = response.data
-        this.tableData.forEach(item => {
-          item.createTime = item.createTime.split('T')[0]
+      appDeploy
+        .initApmPackages()
+        .then((response) => {
+          this.tableData = response.data
+          this.tableData.forEach((item) => {
+            item.createTime = item.createTime.split('T')[0]
+          })
+          this.paginationData = this.tableData
+          this.checkProjectData()
+          if (this.appType) {
+            this.filterTableData(this.appType, 'type')
+          }
+          if (this.serchData) {
+            this.getSearchData(this.serchData)
+          }
+          this.dataLoading = false
         })
-        this.paginationData = this.tableData
-        console.log(this.tableData)
-        this.checkProjectData()
-        if (this.appType) this.filterTableData(this.appType, 'type')
-        if (this.serchData) {
-          this.getSearchData(this.serchData)
-        }
-        this.dataLoading = false
-      }).catch((error) => {
-        console.log(error)
-      })
+        .catch((error) => {
+          console.log(error)
+          this.dataLoading = false
+        })
     },
     checkProjectData () {
-      this.tableData.forEach(itemBe => {
-        INDUSTRY.forEach(itemFe => {
-          if (itemBe.industry.match(itemFe.label[1]) && this.language === 'cn') {
-            itemBe.industry = itemBe.industry.replace(itemFe.label[1], itemFe.label[0])
-          } else if (itemBe.industry.match(itemFe.label[1]) && this.language === 'en') {
-            itemBe.industry = itemBe.industry.replace(itemFe.label[0], itemFe.label[1])
+      this.tableData.forEach((itemBe) => {
+        INDUSTRY.forEach((itemFe) => {
+          if (itemBe.industry.match(itemFe.label[1])) {
+            let _first = this.language === 'cn' ? itemFe.label[1] : itemFe.label[0]
+            let _second = this.language === 'cn' ? itemFe.label[0] : itemFe.label[1]
+            itemBe.industry = itemBe.industry.replace(_first, _second)
           }
         })
-
-        TYPESFORAPP.forEach(itemFe => {
-          if (itemBe.type.match(itemFe.label[1]) && this.language === 'cn') {
-            itemBe.type = itemBe.type.replace(itemFe.label[1], itemFe.label[0])
-          } else if (itemBe.type.match(itemFe.label[1]) && this.language === 'en') {
-            itemBe.type = itemBe.type.replace(itemFe.label[0], itemFe.label[1])
+        TYPESFORAPP.forEach((itemFe) => {
+          if (itemBe.type.match(itemFe.label[1])) {
+            let _first = this.language === 'cn' ? itemFe.label[1] : itemFe.label[0]
+            let _second = this.language === 'cn' ? itemFe.label[0] : itemFe.label[1]
+            itemBe.type = itemBe.type.replace(_first, _second)
           }
         })
       })
     }
-
   }
 }
 </script>
 
 <style lang="less">
-.appdeploy{
+.appdeploy {
   width: 100%;
   height: 100%;
-  padding:32px 15% 0 10%;
-  .deploylist-top-title{
+  padding: 32px 15% 0 10%;
+  .deploylist-top-title {
     width: 186px;
     height: 66px;
     margin: 2% 0 2% 3%;
@@ -194,67 +213,139 @@ export default {
     line-height: 50px;
     text-align: center;
     letter-spacing: 4px;
-    background: url('../../assets/images/mecm/appDeployDetailList/deploy_title.png') no-repeat center;
+    background: url("../../assets/images/mecm/appDeployDetailList/deploy_title.png")
+      no-repeat center;
   }
-  .card-shadow{
+  .card-shadow {
     padding: 0px 0 1px 2px;
     border-radius: 17px;
     border: 1px solid;
     border-color: rgba(182, 164, 236, 0.6);
-    box-shadow: -1px -1px 3px rgba(255, 255, 255,0.3);
-    .el-row{
-      input{
-        color:#fff;
+    box-shadow: -1px -1px 3px rgba(255, 255, 255, 0.3);
+    .el-row {
+      input {
+        color: #fff;
       }
-      .el-input__inner{
-        height:30px;
-        line-height:30px;
-        width:200px;
-        border-radius:6px;
-        border-color:rgba(171,151,229,1);
-        background-color: rgba(182, 164, 236,0.3);
-        box-shadow:-1px -1px 3px rgba(175, 163, 212, 0.6)
+      .el-input__inner {
+        height: 30px;
+        line-height: 30px;
+        width: 200px;
+        border-radius: 6px;
+        border-color: rgba(171, 151, 229, 1);
+        background-color: rgba(182, 164, 236, 0.3);
+        box-shadow: -1px -1px 3px rgba(175, 163, 212, 0.6);
       }
-      .el-input__suffix{
-        right:25px;
-        top:-3px;
+      .el-input__suffix {
+        right: 25px;
+        top: -3px;
       }
     }
-    .el-card{
-      border:1px solid rgba(171,151,229,1);
+    .el-card {
+      border: 1px solid rgba(171, 151, 229, 1);
       border-radius: 17px;
-      padding:35px 16px 7px 24px;
-      background: url('../../assets/images/mecm/deployCommon/deploy_bg.png') center;
-      box-shadow:1px 1px 3px #ab97e5 inset;
-      .el-table{
-        margin-top:30px;
-        margin-bottom:50px;
-        thead{
-          height:60px;
-          border:none;
-          font-size:16px;
+      padding: 35px 16px 7px 24px;
+      background: url("../../assets/images/mecm/deployCommon/deploy_bg.png")
+        center;
+      box-shadow: 1px 1px 3px #ab97e5 inset;
+      .el-loading-mask {
+        background-color: transparent;
+      }
+      .el-pagination {
+        text-align: right;
+        font-weight: normal;
+        .el-pagination__total {
+          float: none;
+          height: 24px;
+          line-height: 24px;
+          font-size: 14px;
+          color: #fff;
         }
-        tr{
-          height:64px;
+        .el-pagination__jump {
+          display: inline-block !important;
+          height: 24px;
+          line-height: 24px;
+          font-size: 14px;
+          color: #fff;
         }
-        td{
-          border-bottom:1px solid rgba(255,255,255,0.3);
-          font-size:14px;
+        .el-pagination__sizes {
+          height: 24px;
+          line-height: 24px;
         }
-        .el-table__cell > .cell{
-          padding-left:30px;
+        .el-select .el-input {
+          height: 24px;
+          line-height: 24px;
         }
-        .el-button{
-          padding:0px 11px;
+        .el-pagination__editor {
+          height: 24px;
         }
-        .el-button--small{
-          font-size:14px;
-          width:auto;
-          height:24px;
-          background: rgba(255,255,255,0.4);
-          border-radius:6px;
+        .el-input--mini {
+          height: 24px;
+          line-height: 22px;
+          font-size: 14px;
         }
+        .el-input__inner {
+          height: 24px;
+          line-height: 24px;
+          font-size: 14px;
+          background: rgba(255, 255, 255, 0.3);
+          color: #fff;
         }
+        .el-input__icon {
+          line-height: 22px;
+        }
+        .btn-prev .el-icon,
+        .btn-next .el-icon {
+          font-size: 14px;
+          height: 24px;
+          line-height: 26px;
+        }
+        .el-pager li {
+          font-size: 14px;
+          font-weight: 200;
+          height: 24px;
+          line-height: 24px;
+          background: none;
+          color: #fff;
+        }
+        button {
+          height: 24px;
+          line-height: 24px;
+          background: none;
+          color: #fff;
+        }
+        .active {
+          background-color: rgba(66, 35, 165, 0.5) !important;
+        }
+      }
+      .el-table {
+        margin-top: 30px;
+        margin-bottom: 50px;
+        thead {
+          height: 60px;
+          border: none;
+          font-size: 16px;
+        }
+        tr {
+          height: 64px;
+        }
+        td {
+          border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+          font-size: 14px;
+        }
+        .el-table__cell > .cell {
+          padding-left: 30px;
+        }
+        .el-button {
+          padding: 0px 11px;
+        }
+        .el-button--small {
+          font-size: 14px;
+          width: auto;
+          height: 24px;
+          background: rgba(255, 255, 255, 0.4);
+          border-radius: 6px;
+        }
+      }
     }
   }
 }
